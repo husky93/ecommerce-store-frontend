dc3d802dce133b6f610a2410041ea1d4eb744425
Maciej Sroka <sroka.maciej93@gmail.com>
2023-01-30 20:48:58 +0100

    Add store route to router settings

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,11 @@ const router = createRouter({
       name: "about",
       component: () => import("../views/AboutView.vue"),
     },
+    {
+      path: "/store",
+      name: "store",
+      component: () => import("../views/StoreView.vue"),
+    },
   ],
 });
 

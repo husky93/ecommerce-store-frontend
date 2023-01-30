1690e3934c8c3743ca04dff83ade815c2b4bc5dc
Maciej Sroka <sroka.maciej93@gmail.com>
2023-01-30 20:49:50 +0100

    Create Category view template

---
+++ src/views/CategoryView.vue
@@ -0,0 +1,14 @@
+<template>
+  <PageLayout>
+    <div class="store">
+      <h1>This is a category page</h1>
+    </div>
+  </PageLayout>
+</template>
+
+<script>
+import PageLayout from "../components/PageLayout.vue";
+export default {
+  components: { PageLayout },
+};
+</script>

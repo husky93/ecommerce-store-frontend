dbbbb0ca8e5259eb2247fe480db294d7ece3f16c
Maciej Sroka <sroka.maciej93@gmail.com>
2023-01-31 09:15:04 +0100

    Initialize Login view file

---
+++ src/views/LoginView.vue
@@ -0,0 +1,14 @@
+<template>
+  <PageLayout>
+    <div class="login">
+      <h1>This is a login page</h1>
+    </div>
+  </PageLayout>
+</template>
+
+<script>
+import PageLayout from "../components/PageLayout.vue";
+export default {
+  components: { PageLayout },
+};
+</script>

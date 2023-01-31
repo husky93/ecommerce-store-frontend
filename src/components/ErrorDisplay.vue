99a3e3d7759b3d4c2de63a16d36d3e1d9e23ee23
Maciej Sroka <sroka.maciej93@gmail.com>
2023-01-31 13:26:26 +0100

    Create Error Display component

---
+++ src/components/ErrorDisplay.vue
@@ -0,0 +1,13 @@
+<template>
+  <div v-if="errorArray">
+    <p v-for="(item, index) in errorArray" :key="item.param + index">
+      {{ item.msg }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["errorArray"],
+};
+</script>

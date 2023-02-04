c10cf5fb28f66b58c2b923688da0ae1144d500e1
Maciej Sroka <sroka.maciej93@gmail.com>
2023-02-04 13:38:00 +0100

    Create custom button template file

---
+++ src/components/CustomButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <button class="btn">{{ text }}</button>
+</template>
+
+<script>
+export default {
+  props: ["text"],
+};
+</script>
+
+<style scoped>
+.btn {
+}
+
+.primary {
+}
+</style>

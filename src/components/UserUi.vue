b5d0f346f688011080b356bbd6d9bf4301db4636
Maciej Sroka <sroka.maciej93@gmail.com>
2023-02-04 13:39:57 +0100

    Create basic header User UI template

---
+++ src/components/UserUi.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <CustomButton class="primary" text="Sign up" />
+    <CustomButton class="primary" text="Login" />
+  </div>
+</template>
+
+<script>
+import CustomButton from "./CustomButton.vue";
+export default {
+  components: {
+    CustomButton,
+  },
+};
+</script>

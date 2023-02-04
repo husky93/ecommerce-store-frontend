d485f73a5951dd6367b5e01a8418af85d196590a
Maciej Sroka <sroka.maciej93@gmail.com>
2023-02-04 13:47:41 +0100

    Redirect to correct routes on click

--- src/components/UserUi.vue
+++ src/components/UserUi.vue
@@ -1,15 +1,30 @@
 <template>
   <div>
-    <CustomButton class="primary" text="Sign up" />
-    <CustomButton class="primary" text="Login" />
+    <CustomButton
+      class="primary"
+      text="Sign up"
+      @click="redirectTo(signupLink)"
+    />
+    <CustomButton class="primary" text="Login" @click="redirectTo(loginLink)" />
   </div>
 </template>
 
 <script>
 import CustomButton from "./CustomButton.vue";
 export default {
+  data() {
+    return {
+      signupLink: "/signup",
+      loginLink: "/login",
+    };
+  },
   components: {
     CustomButton,
+  },
+  methods: {
+    redirectTo(link) {
+      this.$router.push(link);
+    },
   },
 };
 </script>

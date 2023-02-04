f49f18ca5081119cb1d0692b558d1723c4321054
Maciej Sroka <sroka.maciej93@gmail.com>
2023-02-04 13:42:18 +0100

    Add UserUI to page header

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,7 +1,23 @@
 <template>
-  <header><h1>Shop</h1></header>
+  <header class="header">
+    <h1>Shop</h1>
+    <UserUi />
+  </header>
 </template>
 
 <script>
-export default {};
+import UserUi from "./UserUi.vue";
+export default {
+  components: {
+    UserUi,
+  },
+};
 </script>
+
+<style scoped>
+.header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>

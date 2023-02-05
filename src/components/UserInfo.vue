edc19c1db0042b01983cc7ee2f2e5b24edb7c465
Maciej Sroka <sroka.maciej93@gmail.com>
2023-02-05 15:33:07 +0100

    Display logged in user name in page header

---
+++ src/components/UserInfo.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>{{ username }}</div>
+</template>
+
+<script>
+export default {
+  computed: {
+    username() {
+      return `${this.$store.state.user.user.name} ${this.$store.state.user.user.surname}`;
+    },
+  },
+};
+</script>

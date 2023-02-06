be4b59ad3274ad9e818bd2d1c53b72cf70cc987a
Maciej Sroka <sroka.maciej93@gmail.com>
2023-02-06 10:42:27 +0100

    Improve CollapsableList semantics and a11y

--- src/components/CollapsableList.vue
+++ src/components/CollapsableList.vue
@@ -1,11 +1,14 @@
 <template>
-  <ul>
-    <li v-for="item in list" :key="item._id">{{ item.name }}</li>
-  </ul>
+  <li :id="`menu-item-${name}`">
+    {{ name }}
+    <ul :aria-labelledby="`menu-item-${name}`">
+      <li v-for="item in list" :key="item._id">{{ item.name }}</li>
+    </ul>
+  </li>
 </template>
 
 <script>
 export default {
-  props: ["list"],
+  props: ["list", "name"],
 };
 </script>

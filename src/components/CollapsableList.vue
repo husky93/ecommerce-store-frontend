9474bd3978775ca72f7d6ee4e4686860fcfd9898
Maciej Sroka <sroka.maciej93@gmail.com>
2023-02-06 10:55:59 +0100

    Display all categories in collapsable list

--- src/components/CollapsableList.vue
+++ src/components/CollapsableList.vue
@@ -2,7 +2,7 @@
   <li :id="`menu-item-${name}`">
     {{ name }}
     <ul :aria-labelledby="`menu-item-${name}`">
-      <li v-for="item in list" :key="item._id">{{ item.name }}</li>
+      <li v-for="item in list" :key="item._id">{{ item.title }}</li>
     </ul>
   </li>
 </template>
--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -1,16 +1,26 @@
 <template>
   <nav>
     <ul>
-      <CollapsableList name="Categories" />
+      <CollapsableList name="Categories" :list="categories" />
     </ul>
   </nav>
 </template>
 
 <script>
+import { getCategories } from "../app/api";
 import CollapsableList from "./CollapsableList.vue";
+
 export default {
+  data() {
+    return {
+      categories: null,
+    };
+  },
   components: {
     CollapsableList,
+  },
+  async created() {
+    this.categories = await getCategories();
   },
 };
 </script>

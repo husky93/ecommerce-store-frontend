2023cbef21f3a054d33adb0052d164fb85beda0e
Maciej Sroka <sroka.maciej93@gmail.com>
2023-02-06 11:29:39 +0100

    Change menu item id to all lowercase

--- src/components/CollapsableList.vue
+++ src/components/CollapsableList.vue
@@ -1,7 +1,7 @@
 <template>
-  <li :id="`menu-item-${name}`">
+  <li :id="`menu-item-${name.toLowerCase()}`">
     {{ name }}
-    <ul :aria-labelledby="`menu-item-${name}`">
+    <ul :aria-labelledby="`menu-item-${name.toLowerCase()}`">
       <li v-for="item in list" :key="item._id">{{ item.title }}</li>
     </ul>
   </li>

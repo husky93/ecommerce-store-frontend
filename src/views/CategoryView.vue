c9494313981e4752ab43a46965ff9cbc9c6c93c9
Maciej Sroka <sroka.maciej93@gmail.com>
2023-02-06 11:53:16 +0100

    Get individual category data based on params from global state

--- src/views/CategoryView.vue
+++ src/views/CategoryView.vue
@@ -1,7 +1,8 @@
 <template>
   <PageLayout>
     <div class="store">
-      <h1>This is a category page</h1>
+      <h1>This is a {{ category.title }} category page</h1>
+      <p>{{ category.description }}</p>
     </div>
   </PageLayout>
 </template>
@@ -10,5 +11,15 @@
 import PageLayout from "../components/PageLayout.vue";
 export default {
   components: { PageLayout },
+  computed: {
+    category() {
+      const { categories } = this.$store.state;
+      if (categories)
+        return this.$store.state.categories.find(
+          (item) => this.$route.params.id === item._id
+        );
+      return {};
+    },
+  },
 };
 </script>

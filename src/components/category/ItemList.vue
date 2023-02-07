a28d0a5daa9b11382ea7b6742eb202708dd5a403
Maciej Sroka <sroka.maciej93@gmail.com>
2023-02-07 17:11:57 +0100

    Render Item card for each item provided to ItemList component as a prop

--- src/components/category/ItemList.vue
+++ src/components/category/ItemList.vue
@@ -1,9 +1,13 @@
 <template>
-  <div></div>
+  <ItemCard v-for="item in items" :key="item._id" />
 </template>
 
 <script>
+import ItemCard from "./ItemCard.vue";
 export default {
+  components: {
+    ItemCard,
+  },
   props: {
     items: Array,
   },

a8fc85652887d0752843bf4af0564f9d1ae54668
Maciej Sroka <sroka.maciej93@gmail.com>
2023-02-07 17:34:36 +0100

    Display a card element for each item in a category

--- src/components/category/ItemCard.vue
+++ src/components/category/ItemCard.vue
@@ -1,9 +1,26 @@
 <template>
-  <div></div>
+  <figure class="card">
+    <img :src="item.cover_img" :alt="`${item.title} cover`" class="card_img" />
+    <figcaption>{{ item.title }}</figcaption>
+    <div class="card_price">{{ price }}$</div>
+  </figure>
 </template>
 
 <script>
 export default {
   props: ["item"],
+  computed: {
+    price() {
+      return (Math.round(this.item.price_gross * 100) / 100).toFixed(2);
+    },
+  },
 };
 </script>
+
+<style scoped>
+.card_img {
+  width: 250px;
+  height: 300px;
+  object-fit: cover;
+}
+</style>

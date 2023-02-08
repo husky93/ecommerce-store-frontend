2c378decb5ba2e2488f577e27d4f4ccfdaadb4d4
Maciej Sroka <sroka.maciej93@gmail.com>
2023-02-08 16:21:47 +0100

    Add price and add to cart button

--- src/views/ItemView.vue
+++ src/views/ItemView.vue
@@ -1,30 +1,53 @@
 <template>
   <PageLayout>
-    <div class="store" v-if="data">
+    <div class="item" v-if="data">
       <h1>This is an item page with id: {{ $route.params.id }}</h1>
       <img :src="data.cover_img" />
+      <div class="container">
+        <div class="left">
+          <h2>Description:</h2>
+          <p>{{ data.description }}</p>
+        </div>
+        <div class="right">
+          <div class="price">
+            <span>{{ price }}$</span>
+          </div>
+          <CustomButton text="Add to cart" @click="addToCart" />
+        </div>
+      </div>
     </div>
-    <div v-if="!data">Error 404. Item not found.</div>
+    <div class="error" v-if="!data">Error 404. Item not found.</div>
   </PageLayout>
 </template>
 
 <script>
 import PageLayout from "../components/PageLayout.vue";
 import { getItemById } from "../app/api";
-import { useFetchData } from "../composition";
+import { useWatchParams } from "../composition";
+import CustomButton from "../components/CustomButton.vue";
 
 export default {
   setup() {
-    const { data } = useFetchData({
+    const { data } = useWatchParams({
       fetch: getItemById,
     });
     return { data };
   },
-  components: { PageLayout },
+  components: { PageLayout, CustomButton },
   computed: {
     price() {
       return (Math.round(this.data.price_gross * 100) / 100).toFixed(2);
     },
   },
+  methods: {
+    addToCart() {},
+  },
 };
 </script>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: space-between;
+}
+</style>

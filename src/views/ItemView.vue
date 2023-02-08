57af581d10ef1857044bf18accb0f75decf63faa
Maciej Sroka <sroka.maciej93@gmail.com>
2023-02-08 14:54:56 +0100

    Fetch each individual item data when visiting single item route

--- src/views/ItemView.vue
+++ src/views/ItemView.vue
@@ -2,13 +2,27 @@
   <PageLayout>
     <div class="store">
       <h1>This is an item page with id: {{ $route.params.id }}</h1>
+      {{ JSON.stringify(data) }}
     </div>
   </PageLayout>
 </template>
 
 <script>
 import PageLayout from "../components/PageLayout.vue";
+import { getItemById } from "../app/api";
+import { useFetchData } from "../composition";
+import { useRoute } from "vue-router";
+
 export default {
+  setup() {
+    const route = useRoute();
+    console.log(route.params.id);
+    const { data } = useFetchData({
+      id: route.params.id,
+      fetch: getItemById,
+    });
+    return { data };
+  },
   components: { PageLayout },
 };
 </script>

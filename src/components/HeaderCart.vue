37b4b19e264f322f3eec74eec950b446e1bc2010
Maciej Sroka <sroka.maciej93@gmail.com>
2023-02-09 11:21:29 +0100

    Redirect to cart page on cart icon click

--- src/components/HeaderCart.vue
+++ src/components/HeaderCart.vue
@@ -1,5 +1,7 @@
 <template>
-  <font-awesome-icon icon="fa-solid fa-cart-shopping" />
+  <router-link to="/cart">
+    <font-awesome-icon icon="fa-solid fa-cart-shopping" />
+  </router-link>
 </template>
 
 <script>

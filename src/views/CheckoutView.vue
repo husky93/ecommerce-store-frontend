8a4e845723d80759995d14e9fd346ccd636efe01
Maciej Sroka <sroka.maciej93@gmail.com>
2023-02-10 10:38:19 +0100

    Add checkout view file

---
+++ src/views/CheckoutView.vue
@@ -0,0 +1,14 @@
+<template>
+  <PageLayout>
+    <div class="checkout">
+      <h1>This is an checkout page</h1>
+    </div>
+  </PageLayout>
+</template>
+
+<script>
+import PageLayout from "../components/PageLayout.vue";
+export default {
+  components: { PageLayout },
+};
+</script>

d90d1a7bedb185258244545b7bcd0369d2cef3b9
Maciej Sroka <sroka.maciej93@gmail.com>
2023-02-10 10:14:25 +0100

    Add trash can icon

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import { createApp } from "vue";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import { faShoppingCart, faTrashCan } from "@fortawesome/free-solid-svg-icons";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-library.add(faShoppingCart);
+library.add(faShoppingCart, faTrashCan);
 
 import "./assets/main.css";
 

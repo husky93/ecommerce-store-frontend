2b288f6a84f584b1b69e2c20e605901339d0e247
Maciej Sroka <sroka.maciej93@gmail.com>
2023-02-10 09:52:50 +0100

    Display Cart Item component for each item in cart

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -2,13 +2,24 @@
   <PageLayout>
     <div class="cart">
       <h1>This is a cart page</h1>
+      <section class="cart_display">
+        <CartItem v-for="item in cart" :item="item" :key="item._id" />
+      </section>
+      <aside class="cart_ui"></aside>
     </div>
   </PageLayout>
 </template>
 
 <script>
 import PageLayout from "../components/PageLayout.vue";
+import CartItem from "../components/cart/CartItem.vue";
+
 export default {
-  components: { PageLayout },
+  components: { PageLayout, CartItem },
+  computed: {
+    cart() {
+      return this.$store.state.cart;
+    },
+  },
 };
 </script>
